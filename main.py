b5d79144e22a8779531e5b3c370a50901063d43f
Okan Olgun <98118524+okanolgun@users.noreply.github.com>
2024-09-29 01:14:14 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -35,3 +35,13 @@ testing_labels = testing_labels[:4000]
 # To shorten this process, we limited our test images and
 # training images from zero to 20,000 and from zero to 4,000.
 
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(32,32,3)))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(64, (3,3), activation='relu'))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(64,(3,3), activation='relu'))
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10, activation='softmax'))
+
